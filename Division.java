9c704035d02c2ae511dffa1ccaf9b92d2e6f7fe0
codewithnikhilsharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-20 20:59:00 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,16 @@
+class Division{
+
+public static void main(String []args){
+
+System.out.println("The two number is : 6  6  ");
+
+int a = 6;
+
+int b= 6;
+
+int div = a/b;
+
+System.out.println(" The Division of two number is = " + div );
+
+}
+}
