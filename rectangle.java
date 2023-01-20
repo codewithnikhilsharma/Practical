a6c72f96659d232fad45906a94a66321b26a9b71
codewithnikhilsharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-20 21:58:02 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,23 @@
+class rectangle{
+
+public static void main(String []args){
+
+System.out.println("The two number is : 6  6  ");
+
+int l = 6;
+
+int b = 6;
+
+int area = l*b; 
+
+System.out.println(" The area of reactangle is : " );
+System.out.println(area);
+ 
+
+int perameter = 2*(l+b); 
+
+System.out.println(" Peremeter is : " );
+System.out.println(perameter);
+
+}
+}
