1ab95cfbf563205ec8a588210d3eaa2932764033
codewithnikhilsharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-20 20:57:00 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,21 @@
+class Nik{
+
+public static void main(String []args){
+
+System.out.println("The two number is : 6  6  ");
+
+int a = 6;
+
+int b = 6;
+
+int area = a*b; // l*b
+
+int perameter = 2*(a+b); // 2*(a+b)
+
+System.out.println(" The area of reactangle is : " );
+System.out.println(area);
+System.out.println(" Peremeter is : " );
+System.out.println(perameter);
+
+}
+}
