38df87298fbc1f92f9249c228426e037b2e6aa77
codewithnikhilsharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-20 20:52:29 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,16 @@
+class Subtraction{
+
+public static void main(String []args){
+
+System.out.println("The two number is : 5  6  ");
+
+int a = 5;
+
+int b= 6;
+
+int sub = a-b;
+
+System.out.println(" The Subtraction of two number is = " + sub );
+
+}
+}
