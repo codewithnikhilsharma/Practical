cb12829478ac04935709ddc65ef992003c341421
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-22 16:03:17 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+ class Arithmetic {
+ public static void main(String[] args){
+
+         int a = 10;
+         
+         int b = 20;
+     
+         System.out.println(a+b/b-a+a*b);
+     }
+     }
