90a24559ce66f8722c3f1c677a937e4f4cba4e55
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-22 16:09:56 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,15 @@
+class Logical{
+
+public static void main(String args[]){
+
+int a = 2;
+int b = 3;
+
+System.out.println(a>b&&b<a);
+System.out.println(a<b&&b>a);
+
+System.out.println(a<b||b>a);
+System.out.println(a>b||b<a);
+
+}
+}
