3274beb26d9bb0d6e03031e2ac729fd6c92750a9
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-03 09:27:11 +0530

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,18 @@
+package controlStatement;
+
+public class ForEach {
+	
+	public static void main(String[] args) {
+		
+		// declare an array
+		
+		int arr[] = {12 ,32 , 44, 44,444};
+		
+		// using for each 
+		
+		for(int i:arr) {
+			System.out.println(i);
+		}
+	}
+
+}
