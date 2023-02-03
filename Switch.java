baccb3214074a815040512bac0ce3e1f6927ce97
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-03 09:30:26 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,30 @@
+package controlStatement;
+
+public class Switch {
+   
+	public static void main(String[] args) {
+	  
+		int no1 = 30;
+		switch(no1) {
+		
+		case 1:
+			System.out.println(20);
+			break;
+			
+		case 10:
+			System.out.println(40);
+			break;
+			
+		case 3:
+				System.out.println(30);
+				break;	
+		
+		case 4:
+			System.out.println(40);
+			break;
+			
+		default:System.out.println("Invalid Number");		
+				
+		}
+	}	
+}
