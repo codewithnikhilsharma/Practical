1d9cdddc9ea55ec0b7b658014a26041df092d37b
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-11 19:45:39 +0530

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,15 @@
+package Today;
+
+import java.util.Scanner;
+
+public class Greeting {
+  
+	  public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.print("Enter your name: ");
+	    String name = scanner.nextLine();
+	    System.out.println("Hello " + name + ", have a great day!");
+	  }
+	}
+
+
