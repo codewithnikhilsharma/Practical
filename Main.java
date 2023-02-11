261cb38ab2f5c2f4552ae57bf6e9083ba607400f
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-11 19:09:44 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,16 @@
-package controlStatement;
-
-class Student{
-	int age;
-	String name;	
-}
+package Today;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		
-		Student jojo = new Student();
-		jojo.age = 22;
-		jojo.name = "Nikhil";
-		System.out.println(jojo.age + " " + jojo.name);
-		
-		
-		Student jojo1 = new Student();
-		jojo1.age = 23;
-		jojo1.name = "Harry";
-		System.out.println(jojo1.age + " " + jojo1.name);
-			
-	}
-}
+  
+//	public class Main {
+		  public static void main(String[] args) {
+		    int n = 10;
+		    System.out.println("The sum of the first " + n + " natural numbers is " + sumOfFirstNNaturalNumbers(n));
+		  }
+
+		  public static int sumOfFirstNNaturalNumbers(int n) {
+		    return n * (n + 1) / 2;
+		  }
+		}
+
+
