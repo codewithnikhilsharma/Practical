4ca86433e0c8025bb70539090fdc4333397b3561
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-11 19:21:47 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,22 @@
+package Today;
+
+import java.util.Scanner;
+
+public class Function {
+   	
+	  public static void main(String[] args) {
+	  Scanner scanner = new Scanner(System.in);
+	    
+	    System.out.print("Enter a number: ");
+	    int number = scanner.nextInt();
+	    if (isEven(number)) {
+	      System.out.println(number + " is even");
+	    } else {
+	      System.out.println(number + " is odd");
+	    }
+	  }
+
+	  public static boolean isEven(int number) {
+	    return number % 2 == 0;
+	  }
+}
