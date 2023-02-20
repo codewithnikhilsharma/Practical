b82039454e77b9360d8e35aa976f3eb0ffd955a5
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-20 17:37:07 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
-package Today;
+package Now;
 
-public class Main {
-  
-//	public class Main {
-		  public static void main(String[] args) {
-		    int n = 10;
-		    System.out.println("The sum of the first " + n + " natural numbers is " + sumOfFirstNNaturalNumbers(n));
-		  }
+// Arithmetic class
+class Arithmetic {
+	
+ // Define the add method that takes two integers as input and returns their sum
+   int add(int num1, int num2){
+     return num1 + num2;
+ }
+}
 
-		  public static int sumOfFirstNNaturalNumbers(int n) {
-		    return n * (n + 1) / 2;
-		  }
-		}
+//Define the Adder class that inherits from the Arithmetic class
+class Adder extends Arithmetic {
+ }
 
-
+    public class Main {
+	 public static void main(String[] args) {
+	     Adder myAdder = new Adder();
+	     
+	     // Call the add method on the Adder object, passing in two integer arguments
+	     int result = myAdder.add(2, 3);
+	     
+	     // Print the result to the console
+	     System.out.println("The sum is: " + result);
+	 }
+}
