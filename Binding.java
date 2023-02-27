b9ea0bb903e311ce6be62773cd668a5aefbf03b5
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-27 16:55:33 +0530

    Add files via upload

---
+++ Binding.java
@@ -0,0 +1,12 @@
+//package Now;
+//
+//public class Binding {
+//
+//	private void display() {
+//		System.out.println("Something");}
+//	public static void main(String[] args) {
+//		Binding obj = new Binding();
+//		obj.display();
+//	}
+//	}
+
