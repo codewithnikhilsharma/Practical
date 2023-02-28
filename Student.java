787a4f694b829eb00fd1716a5c88cf901e9aff1e
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-28 23:18:27 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -9,15 +9,22 @@ class info{
 		System.out.println(this.name);
 		System.out.println(this.age);
 	}
+	
+	info(){
+		System.out.println(" this is parameterized constructor");
+	}
+	
+	info(String name , int age){
+		this.name = name;
+		this.age = age;
+		
+	}
 }
 
 public class Student {
 public static void main(String[] args) {
 	
-	info s1 = new info();
-	s1.name = "nikhil";
-	s1.age = 22;
-	
+	info s1 = new info(" Nikhil" , 22);
 	s1.Sinfo();
 }
 }
