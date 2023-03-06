56cb4b20c59c11105ea72809bb187cc41d2f27c7
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-06 16:58:46 +0530

    Add files via upload

---
+++ Additionarray.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class Additionarray {
+  public static void main(String[] args) {
+	
+	  // create matrix
+	  int a [][] = {{1 , 2 ,5},{3,4,5}};
+	  int b [][] = {{1 , 2, 2},{3, 1, 5}};
+	  
+	  int c[][] = new int [2][3];
+	  
+	  for(int i =0; i<2; i++) {
+		  for(int j=0; j<3; j++) {
+			  c[i][j] = a[i][j]+b[i][j];
+			  System.out.print(c[i][j]+ " ");
+		  }
+		      System.out.println();
+	  }
+}
+}
