81e62ae786673eb9075e6cd5b2ca8a5964facf15
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-13 17:15:23 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,15 @@
+package NLab;
+
+public class Substring {
+	    public static void main(String[] args) {
+	    	
+	        String str = "Hello World";
+	        
+	        String substr1 = str.substring(6); // returns "World"
+	        String substr2 = str.substring(0, 5); // returns "Hello"
+	        System.out.println("Substring 1: " + substr1);
+	        System.out.println("Substring 2: " + substr2);
+	    }
+	}
+
+
