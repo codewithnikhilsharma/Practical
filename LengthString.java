261a1e9e2b1046f5db26199581172ec0084c3eed
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-13 17:20:28 +0530

    Add files via upload

---
+++ LengthString.java
@@ -0,0 +1,12 @@
+package NLab;
+
+public class LengthString {
+	    public static void main(String[] args) {
+	    	
+	        String str = "Hello World";
+	        int length = str.length();
+	        System.out.println("The length of the string is: " + length);
+	    }
+	}
+
+
