7fa96ca37e4e2bcf4abf22e323cd3cb210f89cd1
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-13 17:37:23 +0530

    Add files via upload

---
+++ Equal.java
@@ -0,0 +1,13 @@
+package NLab;
+
+public class Equal {
+public static void main(String[] args) {
+	
+	String str1 = "Hello";
+	String str2 = "hello";
+	if(str1.equalsIgnoreCase(str2)){
+	    System.out.println("Strings are equal");
+	}
+
+}
+}
