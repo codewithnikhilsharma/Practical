225c68b5a4eec8b9b1e4f08743d453b5e8d31429
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-13 17:30:25 +0530

    Add files via upload

---
+++ StringEqualy.java
@@ -0,0 +1,17 @@
+package NLab;
+
+public class StringEqualy {
+
+	    public static void main(String[] args) {
+	        String str1 = "Hello";
+	        String str2 = "World";
+
+	        if (str1.equals(str2)) {
+	            System.out.println("The strings are equal.");
+	        } else {
+	            System.out.println("The strings are not equal.");
+	        }
+	    }
+	}
+
+
