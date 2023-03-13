a6c001eb6ec54a3fe2bc707d2efc4622311e2c88
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-13 17:20:00 +0530

    Add files via upload

---
+++ Lenstring.java
@@ -0,0 +1,11 @@
+package NLab;
+
+public class Lenstring {
+
+	public static void main(String[] args) {
+    	
+        String str = "Hello World";
+
+        System.out.println("The length of the string is: " + str.length());
+    }
+}
