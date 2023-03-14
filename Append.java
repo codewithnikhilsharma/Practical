7c54db7713eefccc8da53e54832fc077358c946d
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-14 14:24:46 +0530

    Add files via upload

---
+++ Append.java
@@ -0,0 +1,17 @@
+package String;
+
+public class Append {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder("h");
+		
+		sb.append("e");
+		sb.append("l");
+		sb.append("l");
+		sb.append("o");
+		
+		System.out.println(sb);
+		
+	}
+}
