531caac05aa3eb5f8202532350a0fdcf559f1226
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-14 13:04:31 +0530

    Add files via upload

---
+++ CharString.java
@@ -0,0 +1,14 @@
+package String;
+
+public class CharString {
+
+public static void main(String[] args) {
+		
+		//to convert string to char
+		
+		String name = "Nikhil Sharma";		
+		
+		for(int i = 0; i<name.length(); i++)
+		System.out.println(name.charAt(i));
+	}
+}
