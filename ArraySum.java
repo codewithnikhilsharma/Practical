98c9b905187b4ba8b64d3b4ab3c3c477d110aa8e
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-20 16:45:15 +0530

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,15 @@
+package Lab;
+
+public class ArraySum {
+
+	    public static void main(String[] args) {
+	        int[] numbers = { 2, 4, 6, 8, 10 };
+	        int sum = 0;
+	        for (int i = 0; i < numbers.length; i++) {
+	            sum += numbers[i];
+	        }
+	        System.out.println("The sum of the numbers is: " + sum);
+	    }
+	}
+
+
