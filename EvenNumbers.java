f604336ffb23d6c803c6968abd452fb3910d6fb3
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-21 13:24:48 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,21 @@
+package com.Nikhil;
+
+import java.util.Scanner;
+
+public class EvenNumbers {
+            public static void main(String[] args) {
+
+                Scanner in  = new Scanner(System.in);
+                System.out.print(" Enter Your No : ");
+                int no = in.nextInt();
+
+                for (int i = 1; i <= no; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println(  " The Even Number is  : " + i );
+                    }
+                }
+            }
+        }
+
+
+
